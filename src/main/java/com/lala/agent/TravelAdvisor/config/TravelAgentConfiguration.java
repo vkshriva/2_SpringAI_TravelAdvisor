0c82fa8c364836d714aaade847d96e0917f243c3
Varun Kumar Shrivastava <varunshrivastava005@gmail.com>
2025-11-03 15:49:36 +0530

    Remove Olaama dependancy as we have to add advicsor and it is working with ChatBuilder.Builder

--- src/main/java/com/lala/agent/TravelAdvisor/config/TravelAgentConfiguration.java
+++ src/main/java/com/lala/agent/TravelAdvisor/config/TravelAgentConfiguration.java
@@ -1,30 +1,43 @@
 package com.lala.agent.TravelAdvisor.config;
 
 import org.springframework.ai.chat.client.ChatClient;
-import org.springframework.ai.chat.prompt.ChatOptions;
-import org.springframework.ai.ollama.OllamaChatModel;
-import org.springframework.ai.openai.OpenAiChatModel;
+import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
+import org.springframework.ai.chat.memory.InMemoryChatMemoryRepository;
+import org.springframework.ai.chat.memory.MessageWindowChatMemory;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 
 @Configuration
 public class TravelAgentConfiguration {
 
+    /*
+    Configure ChatClient with MessageChatMemoryAdvisor
+    -Uses an in-memory repository
+    -Keeps a window of the last 20 messages
+     */
+
+    @Bean
+    @Primary
+    public ChatClient openAiChatClient(ChatClient.Builder  builder){
+        //in-memory repository (demo only)
+        InMemoryChatMemoryRepository memoryRepository = new InMemoryChatMemoryRepository();
+        //a window that keeps the last N message per conversation
+        MessageWindowChatMemory memory = MessageWindowChatMemory.builder()
+                .chatMemoryRepository(memoryRepository)
+                .maxMessages(20)
+                .build();
+
+        //Create the MesssageChatMemoryAdvisor
+        MessageChatMemoryAdvisor memoryAdvisor = MessageChatMemoryAdvisor.builder(memory).build();
+
+        return builder.defaultAdvisors(memoryAdvisor).build();
+
+    }
+
 //    @Bean
-//    public ChatClient openAiChatClient(ChatClient.Builder builder){
-//        ChatOptions chatOptions = ChatOptions.builder()
-//                .model("gpt-4o-mini")
-//                .build();
-//        return builder.defaultOptions(chatOptions).build();
+//    public ChatClient ollamaChatClient(OllamaChatModel model){
+//       return ChatClient.create(model);wi
 //    }
-
-    @Bean
-    public ChatClient openAiChatClient(OpenAiChatModel model){
-        return ChatClient.create(model);
-    }
-
-    @Bean
-    public ChatClient ollamaChatClient(OllamaChatModel model){
-       return ChatClient.create(model);
-    }
 }
--- src/main/java/com/lala/agent/TravelAdvisor/controller/TravelAgentController.java
+++ src/main/java/com/lala/agent/TravelAdvisor/controller/TravelAgentController.java
@@ -18,32 +18,11 @@ import java.util.Map;
 public class TravelAgentController {
 
     @Autowired
-    @Qualifier("ollamaChatClient")
-    private ChatClient ollamachatClient;
-    @Autowired
-    @Qualifier("openAiChatClient")
     private ChatClient openAiChatClient;
 
     @Value("classpath:prompts/travel-plan-prompt.st")
     private Resource travelTemplate;
 
-    @PostMapping("/recommend-using-ollama")
-    public ResponseEntity<?> recommendTravelPlanUsingOllama(@RequestParam("city") String city, @RequestParam("days") int days) {
-      try{
-          PromptTemplate promptTemplate = new PromptTemplate(travelTemplate);
-          Map<String, Object> variables = Map.of("city", city, "days", days);
-          Prompt promptObj = promptTemplate.create(variables);
-          // String response = chatClient.prompt(promptObj).call().content();
-          TravelResponse response = ollamachatClient.prompt()
-                  .system("You are a travel planning assistant and you are an API that return a single valid JSON object")
-                    .user(promptObj.getContents())
-                    .call()
-                    .entity(TravelResponse.class);
-            return ResponseEntity.ok(response);
-        } catch (Exception e){
-            return ResponseEntity.status(500).body("Error generating travel plan: " + e.getMessage());
-      }
-    }
 
     @PostMapping("/recommend-using-openai")
     public ResponseEntity<?> recommendTravelPlanUsingOpenAI(@RequestParam("city") String city, @RequestParam("days") int days) {
