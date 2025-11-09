239971f1ac510d73b0366771e7458d99930b9dd2
Varun Kumar Shrivastava <varunshrivastava005@gmail.com>
2025-11-09 21:52:31 +0530

    Replace POJO with Record

--- src/main/java/com/lala/agent/TravelAdvisor/dto/response/Itinerary.java
+++ src/main/java/com/lala/agent/TravelAdvisor/dto/response/Itinerary.java
@@ -1,36 +1,10 @@
 package com.lala.agent.TravelAdvisor.dto.response;
 
-
-
 import java.util.List;
 
+public record Itinerary(
+    int day,
+    List<String> activities,
+    String food
+) {}
 
-public class Itinerary {
-    private int day;
-    private List<String> activities;
-    private String food;
-
-    public int getDay() {
-        return day;
-    }
-
-    public void setDay(int day) {
-        this.day = day;
-    }
-
-    public List<String> getActivities() {
-        return activities;
-    }
-
-    public void setActivities(List<String> activities) {
-        this.activities = activities;
-    }
-
-    public String getFood() {
-        return food;
-    }
-
-    public void setFood(String food) {
-        this.food = food;
-    }
-}
